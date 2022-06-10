7c66074b4229f44d99f33cdcac2dd4a174524aed
Marques Jordan <marques.jordan81@gmail.com>
2022-06-09 23:49:01 -0700

    Remove header

--- index.js
+++ index.js
@@ -33,15 +33,6 @@ app.use(function (req, res, next) {
   }
 });
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://bipoc-web.vercel.app'); // update to match the domain you will make the request from
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept',
-  );
-  next();
-});
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
