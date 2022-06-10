0a6493ffe71c11a9cbe7fe47405f23feff69c1da
Marques Jordan <marques.jordan81@gmail.com>
2022-06-09 23:33:17 -0700

    CORS fix

--- index.js
+++ index.js
@@ -26,11 +26,12 @@ mongoose.connection.on('error', (err) => {
 const app = express();
 
 app.use(function (req, res, next) {
-  if (req.headers['x-forwarded-proto'] == 'http') {
-    res.redirect('https://bipoc-web.vercel.app' + req.url);
-  } else {
-    return next();
-  }
+  res.header('Access-Control-Allow-Origin', 'https://bipoc-web.vercel.app'); // update to match the domain you will make the request from
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  next();
 });
 
 app.use(bodyParser.json());
