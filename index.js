6217b5dfbd14c9c1bea87795b2154f4ed0126d4b
Marques Jordan <marques.jordan81@gmail.com>
2022-06-13 12:27:22 -0700

    Change CORS

--- index.js
+++ index.js
@@ -32,10 +32,7 @@ app.use(
 );
 
 const allowCrossDomain = function (req, res, next) {
-  res.header(
-    'Access-Control-Allow-Origin',
-    'https://bipoc-web-marquesjordan.vercel.app',
-  );
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
@@ -43,7 +40,7 @@ const allowCrossDomain = function (req, res, next) {
 app.use(allowCrossDomain);
 app.use(
   cors({
-    origin: 'https://bipoc-web-marquesjordan.vercel.app',
+    origin: '*',
     credentials: true,
   }),
 );
